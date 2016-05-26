3cae1da163f21414c8f2a74c3fbd8899612bc964
Kyle Machulis <kyle@nonpolynomial.com>
2016-05-25 19:43:53 -0700

    Converted definitions to HTML parse mode

--- metafetishbot/definitions.py
+++ metafetishbot/definitions.py
@@ -1,6 +1,7 @@
 import os
 from telegram.ext import CommandHandler
 from .pickledb import pickledb
+import cgi
 
 
 class DefinitionManager(object):
@@ -16,58 +17,44 @@ class DefinitionManager(object):
         dispatcher.add_handler(CommandHandler('def_add', self.add))
         dispatcher.add_handler(CommandHandler('def_rm', self.rm))
 
-    def _contains_special_chars(self, defstr):
-        """Returns True if string contains *[]()_`"""
-        for s in defstr:
-            # Should probably do this as a regexp but eh.
-            if any(c in s for c in ['(', ')', '*', '[', ']', '_', '`']):
-                return True
-        return False
-
-    def send_char_error(self, bot, update):
+    def help(self, bot, update):
         bot.sendMessage(update.message.chat_id,
-                        text='The following characters are not allowed in definition queries: []()*_`')
+                        text="This is not helpful.")
 
     def show(self, bot, update):
-        def_name = update.message.text.partition(" ")[2].strip().lower()
-        if self._contains_special_chars([def_name]):
-            self.send_char_error(bot, update)
-            return
+        def_name = cgi.escape(update.message.text.partition(" ")[2].strip().lower())
         if self.db.get(def_name) is None:
             bot.sendMessage(update.message.chat_id,
                             text='No definition available for %s' %
                             (def_name),
-                            parse_mode="Markdown")
+                            parse_mode="HTML")
             return
-        def_str = "Definition for _%s_:\n" % (def_name)
+        def_str = "Definition for <i>%s</i>:\n" % (def_name)
         i = 1
         def_text = self.db.lgetall(def_name)
         for d in def_text:
-            def_str += "*%d.* %s\n" % (i, d["desc"])
+            def_str += "<b>%d.</b> %s\n" % (i, d["desc"])
             i += 1
         bot.sendMessage(update.message.chat_id,
                         text=def_str,
-                        parse_mode="Markdown")
+                        parse_mode="HTML")
 
     def add(self, bot, update):
         command = update.message.text.partition(" ")[2]
         (def_name, def_part, def_add) = command.partition(" ")
-        def_name = def_name.strip().lower()
-        def_add = def_add.strip()
-        if self._contains_special_chars([def_name, def_add]):
-            self.send_char_error(bot, update)
-            return
+        def_name = cgi.escape(def_name.strip().lower())
+        def_add = cgi.escape(def_add.strip())
         if self.db.get(def_name) is None:
             bot.sendMessage(update.message.chat_id,
-                            text='Adding definition:\n_%s_\n for term _%s_.' %
+                            text='Adding definition:\n<i>%s</i>\n for term <i>%s</i>.' %
                             (def_add, def_name),
-                            parse_mode="Markdown")
+                            parse_mode="HTML")
             self.db.lcreate(def_name)
         else:
             bot.sendMessage(update.message.chat_id,
-                            text='Definition for _%s_ already exists, extending with definition _%s_.' %
+                            text='Definition for <i>%s</i> already exists, extending with definition <i>%s</i>.' %
                             (def_name, def_add),
-                            parse_mode="Markdown")
+                            parse_mode="HTML")
         d = {"user": update.message.from_user.id,
              "desc": def_add.strip()}
         self.db.ladd(def_name, d)
@@ -75,31 +62,28 @@ class DefinitionManager(object):
     def rm(self, bot, update):
         command = update.message.text.partition(" ")[2]
         (def_name, def_part, def_rm) = command.partition(" ")
-        def_name = def_name.strip().lower()
-        if self._contains_special_chars([def_name, def_rm]):
-            self.send_char_error(bot, update)
-            return
+        def_name = cgi.escape(def_name.strip().lower())
         if self.db.get(def_name) is None:
             bot.sendMessage(update.message.chat_id,
-                            text='No definition available for _%s_' %
+                            text='No definition available for <i>%s</i>' %
                             (def_name),
-                            parse_mode="Markdown")
+                            parse_mode="HTML")
             return
         try:
             def_id = int(def_rm) - 1
             self.db.lpop(def_name, def_id)
         except:
             bot.sendMessage(update.message.chat_id,
-                            text='Index %s is not valid for definition _%s_.' %
+                            text='Index %s is not valid for definition <i>%s</i>.' %
                             (def_rm, def_name),
-                            parse_mode="Markdown")
+                            parse_mode="HTML")
             return
         if self.db.llen(def_name) is 0:
             self.db.lrem(def_name)
         bot.sendMessage(update.message.chat_id,
-                        text='Index %s for definition _%s_ deleted.' %
+                        text='Index %s for definition <i>%s</i> deleted.' %
                         (def_rm, def_name),
-                        parse_mode="Markdown")
+                        parse_mode="HTML")
 
     def shutdown(self):
         self.db.dump()
